05bf74146bddab9459ff3b1ef12fd266a1e47b0f
Usaid <ansariusaid005@gmail.com>
2026-09-17 03:57:48 +0000

    solve binary search algorithm based problem

---
+++ arrays/BinarySearch1.java
@@ -0,0 +1,31 @@
+package arrays;
+
+public class BinarySearch1 {
+    public static void main(String[] args) {
+        int[] arr = { -21, 5, 6, 8, 41, 52, 83, 92, 95, 100 };
+        int target = -21;
+        int ans = binarySearch(arr, target);
+        System.out.println("The target index is : " + ans);
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+
+}
