8014783542f27fd1cf41591e9d81a62c670d2ef1
Usaid <ansariusaid005@gmail.com>
2026-09-21 15:52:58 +0000

    Solve three pattern problem

---
+++ patternprinting/Pattern3.java
@@ -0,0 +1,18 @@
+
+
+public class Pattern3{
+    public static void main(String[] args) {
+        star3(5);
+    }
+    static void star3(int n){
+        for(int row = n; row >= 1; row--){
+
+            for(int col = 1; col <= row ; col++){
+
+                System.out.print("* "); //* * * * *  
+
+            }
+            System.out.println();
+        }
+    }
+}
