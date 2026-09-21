ba175183743e058d66371443734d314d584ec782
Usaid <ansariusaid005@gmail.com>
2026-09-21 16:52:56 +0000

    Add another method to solve the same problem

--- patternprinting/Pattern3.java
+++ patternprinting/Pattern3.java
@@ -1,18 +1,29 @@
 
-
-public class Pattern3{
+public class Pattern3 {
     public static void main(String[] args) {
         star3(5);
     }
-    static void star3(int n){
-        for(int row = n; row >= 1; row--){
 
-            for(int col = 1; col <= row ; col++){
+    static void star3(int n) {
+        // for(int row = n; row >= 1; row--){
 
-                System.out.print("* "); //* * * * *  
+        // for(int col = 1; col <= row ; col++){
 
+        // System.out.print("* "); //* * * * *
+
+        // }
+        // System.out.println();
+        // }
+        // }
+
+        // One another method to print the same pattern
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row + 1; col++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
+
     }
 }
