899b2de07723bc030cc31267c33952397bd1fcfd
Usaid <ansariusaid005@gmail.com>
2026-09-21 14:26:48 +0000

    Solve second pattern problem

---
+++ patternprinting/Pattern2.java
@@ -0,0 +1,14 @@
+public class Pattern2{
+    public static void main(String[] args) {
+        pattern2(5);
+    }
+    static void  pattern2(int n) {
+        for(int row = 1; row <= n; row++){
+            // print stars for each row
+            for(int col = 1; col <= row; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
