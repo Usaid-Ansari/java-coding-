420867671fd62ad6d7a7f9711eda9c8a635e16c3
Usaid <ansariusaid005@gmail.com>
2026-09-21 10:59:02 +0000

    Update floor number problem

--- arrays/FloorNumber.java
+++ arrays/FloorNumber.java
@@ -2,12 +2,12 @@ package arrays;
 
 public class FloorNumber {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 5, 9, 14, 16, 18};
-        int target = 15;
+       int [] arr = {2, 5, 8, 12, 16, 20};
+       int target = 13;
         int ans = floorNumber(arr, target);
         System.out.println("The target index is : " + ans);
     }
-   // return the index of smallest no >= target
+   // return the index of smallest no <= target
     static int floorNumber(int[] arr, int target) {
 
         if(target > arr[arr.length -1]) {
@@ -33,3 +33,12 @@ public class FloorNumber {
     }
 
 }
+
+
+
+
+
+
+
+
+
